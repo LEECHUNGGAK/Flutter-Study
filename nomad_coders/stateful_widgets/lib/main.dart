dad4fe6604bd86d03921b5ce9a72d65b2b83289a
Lee Chung Gak <lechga@yahoo.com>
2025-06-03 23:33:00 +0900

    Complete Stateful Widgets

--- nomad_coders/stateful_widgets/lib/main.dart
+++ nomad_coders/stateful_widgets/lib/main.dart
@@ -12,30 +12,81 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  List<int> numbers = [];
+  bool showTitle = true;
 
   void onClicked() {
-    counter = counter + 1;
+    setState(() {
+      numbers.add(numbers.length);
+    });
+  }
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(titleLarge: TextStyle(color: Colors.red)),
+      ),
       home: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Click Count", style: TextStyle(fontSize: 30)),
-              Text("$counter", style: TextStyle(fontSize: 30)),
+              showTitle ? MyTitle() : Text("Nothing"),
+              for (var elem in numbers) Text("$elem"),
               IconButton(
                 iconSize: 40,
                 onPressed: onClicked,
                 icon: Icon(Icons.add_box),
               ),
+              IconButton(
+                iconSize: 40,
+                onPressed: toggleTitle,
+                icon: Icon(Icons.remove_red_eye),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyTitle extends StatefulWidget {
+  const MyTitle({
+    super.key,
+  });
+
+  @override
+  State<MyTitle> createState() => _MyTitleState();
+}
+
+class _MyTitleState extends State<MyTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print("MyTitle initState");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("MyTitle dispose");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Click Count",
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge!.color,
       ),
     );
   }
