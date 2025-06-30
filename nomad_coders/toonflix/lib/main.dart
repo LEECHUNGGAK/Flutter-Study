26d60bd03fbc2252327b347c9cfdd1cbc20a7be4
Lee Chung Gak <chgale@outlook.kr>
2025-06-30 18:01:18 +0900

    Add home_screen.dart

--- nomad_coders/toonflix/lib/main.dart
+++ nomad_coders/toonflix/lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Hello Flutter")),
-        body: Center(child: Text("Hello World")),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFE7626C),
+        textTheme: TextTheme(displayLarge: TextStyle(color: Color(0xFF232B55))),
+        cardColor: Color(0xFFF4EDDB),
       ),
+      home: HomeScreen(),
     );
   }
 }
